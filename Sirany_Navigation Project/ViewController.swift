a9c991bb41c4c7e18f9eb29f7d47111cab5c270a
Peter Sirany <peterCFII@hotmail.com>
2020-07-22 22:11:21 +0400

    Initial Commit

---
+++ Sirany_Navigation Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sirany_Navigation Project
+//
+//  Created by Peter Sirany on 7/22/20.
+//  Copyright © 2020 Peter Sirany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
